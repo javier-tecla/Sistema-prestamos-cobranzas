accecc03eef583366d1fa33dd0b0c22d08c90706
Javier Borjas <cristman11@gmail.com>
2026-02-17 14:03:01 -0300

    Instalación de Laravel Spatie Permissions: Roles y Permisos

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+ 
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,11 +9,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
