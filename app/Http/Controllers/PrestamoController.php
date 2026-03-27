3746d79f2e7f03b56b56fbc9dc1e00fa31447846
Javier Borjas <cristman11@gmail.com>
2026-03-26 23:48:55 -0300

    Registro Automática de Cuotas, Insertando la Tabla de Amortización en la BD

--- app/Http/Controllers/PrestamoController.php
+++ app/Http/Controllers/PrestamoController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Ajuste;
 use App\Models\Categoria;
 use App\Models\Cliente;
+use App\Models\Pago;
 use App\Models\Prestamo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PrestamoController extends Controller
 {
@@ -16,6 +18,7 @@ class PrestamoController extends Controller
     public function index()
     {
         $prestamos = Prestamo::paginate(10);
+
         return view('admin.prestamos.index', compact('prestamos'));
     }
 
@@ -27,6 +30,7 @@ class PrestamoController extends Controller
         $clientes = Cliente::all();
         $categorias = Categoria::all();
         $ajuste = Ajuste::first();
+
         return view('admin.prestamos.create', compact('clientes', 'categorias', 'ajuste'));
     }
 
@@ -47,25 +51,61 @@ class PrestamoController extends Controller
             'monto_interes_total' => 'required|numeric|min:0',
             'monto_total_a_pagar' => 'required|numeric|min:0',
             'fecha_inicio' => 'required|date',
+            'cuotas_json' => 'required|json',
         ]);
 
-        $prestamo = new Prestamo();
-        $prestamo->monto_prestado = $request->monto_prestado;
-        $prestamo->cliente_id = $request->cliente_id;
-        $prestamo->categoria_id = $request->categoria_id;
-        $prestamo->tasa_interes = $request->tasa_interes;
-        $prestamo->modalidad_pago = $request->modalidad_pago;
-        $prestamo->modalidad_amortizacion = $request->modalidad_amortizacion;
-        $prestamo->nro_cuotas = $request->nro_cuotas;
-        $prestamo->monto_interes_total = $request->monto_interes_total;
-        $prestamo->monto_total_a_pagar = $request->monto_total_a_pagar;
-        $prestamo->fecha_inicio = $request->fecha_inicio;
-        $prestamo->estado = 'Pendiente';
-        $prestamo->save();
+        try {
+            DB::beginTransaction();
 
-        return redirect()->route('admin.prestamos.index')
-            ->with('mensaje', 'Préstamo creado exitosamente')
-            ->with('icono', 'success');
+            $prestamo = new Prestamo;
+            $prestamo->monto_prestado = $request->monto_prestado;
+            $prestamo->cliente_id = $request->cliente_id;
+            $prestamo->categoria_id = $request->categoria_id;
+            $prestamo->tasa_interes = $request->tasa_interes;
+            $prestamo->modalidad_pago = $request->modalidad_pago;
+            $prestamo->modalidad_amortizacion = $request->modalidad_amortizacion;
+            $prestamo->nro_cuotas = $request->nro_cuotas;
+            $prestamo->monto_interes_total = $request->monto_interes_total;
+            $prestamo->monto_total_a_pagar = $request->monto_total_a_pagar;
+            $prestamo->fecha_inicio = $request->fecha_inicio;
+            $prestamo->estado = 'Pendiente';
+            $prestamo->save();
+
+            $cuotas = json_decode($request->cuotas_json, true) ?: [];
+            if(count($cuotas) !== (int) $request->nro_cuotas) {
+               throw new \Exception('El número de cuotas no coincide con el número de cuotas generado');
+            }
+
+
+            $numero_cuota = 1;
+            foreach ($cuotas as $cuota) {
+                $pago = new Pago();
+                $pago->prestamo_id = $prestamo->id;
+                $pago->fecha_vencimiento = $cuota['fecha_vencimiento'];
+                $pago->saldo_capital = $cuota['saldo_capital'];
+                $pago->monto_capital = $cuota['monto_capital'];
+                $pago->monto_interes = $cuota['monto_interes'];
+                $pago->monto_cuota = $cuota['monto_cuota'];
+                $pago->metodo_pago = '-';
+                $pago->referencia_pago = 'Cuota:' . $numero_cuota;
+                $pago->save();
+                $numero_cuota++;
+            }
+
+            DB::commit();
+
+             return redirect()->route('admin.prestamos.index')
+                ->with('mensaje', 'Préstamo creado exitosamente')
+                ->with('icono', 'success');
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return redirect()->back()
+                ->with('mensaje', 'Error al crear el préstamo: '.$e->getMessage())
+                ->with('icono', 'error');
+        }
+
     }
 
     /**
